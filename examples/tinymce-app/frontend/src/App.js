2970c036e1c8d7cde94610b6f5b5dec2bf0726c2
zhongyi.li <1454598684@qq.com>
2022-12-04 16:18:07 +0800

    feat: 引入 tinymce 富文本编辑器

--- examples/tinymce-app/frontend/src/App.js
+++ examples/tinymce-app/frontend/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import TinymceEditor from './components/TinymceEditor/index';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TinymceEditor></TinymceEditor>
+    </div>
+  );
 }
 
 export default App;
