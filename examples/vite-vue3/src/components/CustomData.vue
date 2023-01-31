22c4fcb52cda67d9156cfa97774978afdadaf91e
zhongyi.li <1454598684@qq.com>
2023-01-31 21:15:03 +0800

    feat: add vue3-vite

---
+++ examples/vite-vue3/src/components/CustomData.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: 'CustomData',
+  data() {
+    return {open: false};
+  },
+  mounted() {
+    this.open = true;
+  }
+};
+</script>
+
+<template>
+  <div>
+    <div id="modal"></div>
+    <p>This is the component content</p>
+    <!-- 必须先存在 #modal DOM -->
+    <Teleport v-if="open" to="#modal">
+      <p>This content will be rendered in the #modal element</p>
+    </Teleport>
+  </div>
+</template>
